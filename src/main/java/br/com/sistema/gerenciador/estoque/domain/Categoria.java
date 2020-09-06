0500874e77c6bee23b7d5d576da0a03b2029e3d4
Pedro Henrique Fernandes <pehfernandesl@gmail.com>
2020-09-05 22:27:43 -0300

    Remove boilerplate das entidades existentes usando o lombok

--- src/main/java/br/com/sistema/gerenciador/estoque/domain/Categoria.java
+++ src/main/java/br/com/sistema/gerenciador/estoque/domain/Categoria.java
@@ -1,72 +1,29 @@
 package br.com.sistema.gerenciador.estoque.domain;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
+@Data
 @Entity
-public class Categoria implements Serializable{
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	private String descricao;
-	
-	public Categoria() {
-		
-	}
+    private static final long serialVersionUID = 1L;
 
-	public Categoria(Integer id, String descricao) {
-		this.id = id;
-		this.descricao = descricao;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	public Integer getId() {
-		return id;
-	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	
-	public String getDescricao() {
-		return descricao;
-	}
-	
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Categoria other = (Categoria) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+    @Column
+    private String descricao;
+
 }
